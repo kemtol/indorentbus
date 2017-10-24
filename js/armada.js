44a4303c4dc9d378bc5e6aad9470158a7f4c3b29
Errol Lenovo G40 <errol.widhavian@rocktokom.co>
2017-10-24 16:13:55 +0700

    checkBox
    Signed-off-by: Errol Lenovo G40 <errol.widhavian@rocktokom.co>

--- js/armada.js
+++ js/armada.js
@@ -6,10 +6,9 @@ jQuery(document).ready(function($)
 	facilities.on('child_added', snap => {
 		var checkBox = '<div class="checkbox">';
 		checkBox += '	<label>';
-		checkbox += '		<input type="'+snap.val()+'" />';
+		checkBox += '		<input type="'+snap.key()+'" /> '+snap.val();
 		checkBox += '	</label>';
 		checkBox += '</div>';
-		console.log(snap);	
 		$('.facilities').append(checkBox);
 	});
 });
