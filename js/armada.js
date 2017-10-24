9eef09e625fd61ed64e55b14e9ef589d1f3e848a
Errol Lenovo G40 <errol.widhavian@rocktokom.co>
2017-10-24 16:35:10 +0700

    lir.moved
    Signed-off-by: Errol Lenovo G40 <errol.widhavian@rocktokom.co>

--- js/armada.js
+++ js/armada.js
@@ -4,12 +4,22 @@ jQuery(document).ready(function($)
 	var facilities = dbRef.child('fleet_facilities');
 	
 	facilities.on('child_added', snap => {
-		var checkBox = '<div class="checkbox">';
+		var checkBox = '<div class="checkbox" id="'+snap.key+'Parent">';
 		checkBox += '	<label>';
-		checkBox += '		<input type="checkbox" name="facilities[]" value="'+snap.key+'"/> '+snap.val();
+		checkBox += '		<input type="checkbox" name="facilities[]" value="'+snap.key+'"/> <span id="'+snap.key+'">'+snap.val()+'</span>';
 		checkBox += '	</label>';
 		checkBox += '</div>';
 		$('.facilities').append(checkBox);
 		console.log(snap)
+	});
+
+	facilities.on('child_changed', snap => {
+		var liChanged = '#'+snap.key;
+		$(liChanged).text(snap.val();
+	});
+
+	facilities.on('child_removed', snap => {
+		var liRemoved = '#'+snap.key+'Parent';
+		$(liRemoved).remove();
 	});
 });
