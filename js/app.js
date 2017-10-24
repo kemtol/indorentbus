e0e59a62c5302af59351b3cb6e207f63ad0fdcf1
Errol Lenovo G40 <errol.widhavian@rocktokom.co>
2017-10-24 12:42:07 +0700

    app.js
    Signed-off-by: Errol Lenovo G40 <errol.widhavian@rocktokom.co>

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCzyFRMOT1r1JF8mM6tZpaJjMI7Gms9oyk",
+	authDomain: "indorentbus-e66dc.firebaseapp.com",
+	databaseURL: "https://indorentbus-e66dc.firebaseio.com",
+	projectId: "indorentbus-e66dc",
+	storageBucket: "indorentbus-e66dc.appspot.com",
+	messagingSenderId: "1065904701560"
+};
+firebase.initializeApp(config);
