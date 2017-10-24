6abfb225d0c70b5c0bccf9c4448beb01999ed413
delpierrol <errol.widhavian@gmail.com>
2017-10-24 16:06:21 +0700

    Update armada.js

--- js/armada.js
+++ js/armada.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($)
 	var dbRef = firebase.database().ref();
 	var facilities = dbRef.child('fleet_facilities');
 	
-	facilities.on('value', snap => {
+	facilities.on('child_added', snap => {
 		console.log(snap.val());	
 	});
 });
