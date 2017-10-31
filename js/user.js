f53b9f5ba00029aa1ae5a641b2da794429d45e1f
Errol Lenovo G40 <errol.widhavian@rocktokom.co>
2017-10-31 15:08:42 +0700

    userData
    Signed-off-by: Errol Lenovo G40 <errol.widhavian@rocktokom.co>

--- js/user.js
+++ js/user.js
@@ -1,55 +1,30 @@
 jQuery(document).ready(function($)
 {
 	var dbRef = firebase.database().ref();
-	var facilities = dbRef.child('fleet_facilities');
-	var types = dbRef.child('fleet_type');
+	var vendors = dbRef.child('vendors');
 	
-	facilities.on('child_added', snap => {
-		var checkBox = '<div class="checkbox" id="'+snap.key+'Parent">';
-		checkBox += '	<label>';
-		checkBox += '		<input type="checkbox" name="fleet_facilities" value="'+snap.val()+'"/> <span id="'+snap.key+'">'+snap.val()+'</span>';
-		checkBox += '	</label>';
-		checkBox += '</div>';
-		$('.facilities').append(checkBox);
-	});
-
-	facilities.on('child_changed', snap => {
-		var liChanged = '#'+snap.key;
-		$(liChanged).text(snap.val());
-	});
-
-	facilities.on('child_removed', snap => {
-		var liRemoved = '#'+snap.key+'Parent';
-		$(liRemoved).remove();
-	});
-
-	
-	types.on('child_added', snap => {
+	vendors.on('child_added', snap => {
 		var option = '<option value="'+snap.val()+'" id="'+snap.key+'">'+snap.val()+'</option>';
-		$('#fleet_type').append(option);
+		$('#user_vendor').append(option);
 	});
 
-	types.on('child_changed', snap => {
+	vendors.on('child_changed', snap => {
 		var optionChanged = '#'+snap.key;
 		$(optionChanged).text(snap.val());
 	});
 
-	types.on('child_removed', snap => {
+	vendors.on('child_removed', snap => {
 		var optionRemoved = '#'+snap.key;
 		$(optionRemoved).remove();
 	});
 
 	$('#newFleet').submit(function(event){
-		var fleet = 
+		var userData = 
 		{
-			'fleet_no'			: $('#fleet_no').val(),
-			'fleet_type'		: $('#fleet_type').val(),
-			'fleet_seat'		: $('#fleet_seat').val(),
-			'fleet_year'		: $('#fleet_year').val(),
-			'fleet_engine'		: $('#fleet_engine').val(),
-			'fleet_stnk'		: $('#fleet_stnk').val(),
-			'fleet_no_rangka'	: $('#fleet_no_rangka').val(),
-			'fleet_image'		: $('#preview').attr('src'),
+			'user_name'			: $('#user_name').val(),
+			'user_email'		: $('#user_email').val(),
+			'user_phone'		: $('#user_phone').val(),
+			'user_vendor'		: $('#user_vendor').val(),
 		};
 
 
@@ -67,35 +42,9 @@ jQuery(document).ready(function($)
 			}
 		});
 
-		var checked_facility = $('input[name=fleet_facilities]:checked');
-		if(checked_facility.length>0)
-		{
-			$('.facilities').find('.help-block').remove();
-			facilities = [];
-			checked_facility.each(function(){
-				facilities.push($(this).val());
-			});
-			fleet.fleet_facilities = facilities;
-		}
-		else
-		{
-			if($('.facilities').find('.help-block').length==0) $('.facilities').append('<span class="help-block" style="font-size:11px;">Fasilitas harus dipilih minimal 1.</span>');
-			if(!danger) 
-			{
-				$('.facilities').find('input[type="checkbox"]').focus();
-				danger = true;
-			}
-		}
-
-		if(!danger)	saveArmada(fleet);
+		if(!danger)	saveUser(userData);
 
 		return false;
-	});
-
-	$(document).on('change','#fleet_image',function(e)
-	{
-		var target = $(this).parent().find('#preview');
-		readURL(this,target);
 	});
 
 	$(document).on('change','.required',function(e)
@@ -107,35 +56,22 @@ jQuery(document).ready(function($)
 
 });
 
-function saveArmada(fleet)
+function saveUser(userData)
 {
-	var fleetData = {
-		author			: currentUser.displayName,
-		uid				: currentUser.uid,
-		fleet_no		: fleet.fleet_no,
-		fleet_type		: fleet.fleet_type,
-		fleet_seat		: fleet.fleet_seat,
-		fleet_year		: fleet.fleet_year,
-		fleet_engine	: fleet.fleet_engine,
-		fleet_stnk		: fleet.fleet_stnk,
-		fleet_no_rangka	: fleet.fleet_no_rangka,
-		fleet_image		: fleet.fleet_image,
-	};
-
-	console.log(fleetData);
+	console.log(userData);
 
 	//firebase.database().ref('fleets').set(fleetData);
 
 	// Get a key for a new F;eet.
-  	var newFleetKey = firebase.database().ref().child('fleets').push().key;
-  	console.log(newFleetKey);
-  	// firebase.database().ref('fleets/' + newFleetKey).set(fleetData);
-  	// Write the new post's data simultaneously in the posts list and the user's post list.
-	var updates = {};
-	updates['/fleets/' + newFleetKey] = fleetData;
-	// updates['/user-fleets/' + uid + '/' + newFleetKey] = fleetData;
+ //  	var newFleetKey = firebase.database().ref().child('fleets').push().key;
+ //  	console.log(newFleetKey);
+ //  	// firebase.database().ref('fleets/' + newFleetKey).set(fleetData);
+ //  	// Write the new post's data simultaneously in the posts list and the user's post list.
+	// var updates = {};
+	// updates['/fleets/' + newFleetKey] = fleetData;
+	// // updates['/user-fleets/' + uid + '/' + newFleetKey] = fleetData;
 
-	firebase.database().ref().update(updates);
+	// firebase.database().ref().update(updates);
 
 	  //window.location = "{{ site.url }}/armada/tambah/?success=1";
 }
